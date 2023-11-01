8cdeabcc9c86a62491af62a9a9323c6144e15c1e
SAFE HACKER <137111516+MrDarknetUz@users.noreply.github.com>
2023-11-01 22:34:20 +0500

    1-Dars
    Python Dasturlash 1-Dars

---
+++ 1-Dars.py
@@ -0,0 +1,47 @@
+#Telegram: t.me/xakkerjamoa
+#You Tube: youtube.com/PROGRAMMINGUZ
+#GitHub: github.com/MrDarknetUz
+
+
+
+#Python syntax
+'''
+print("Hello, World!")
+'''
+
+
+
+#comments
+'''
+#bu komment
+Bu komment
+'''
+
+
+
+#creating variables
+'''
+a=5
+print(a)
+'''
+  
+  
+  
+#data types
+'''
+int => integer {-1,-2...1,2...}
+str => string {"a","b","c","-","@","+","("...}
+float => float {2.1,1.3...}
+bool => boolean {True, False}
+'''
+
+
+
+#function type()
+'''
+a=1
+b="hello"
+c=1.0
+d=True
+print("a =>",type(a),"\nb =>",type(b),"\nc =>",type(c),"\nd =>",type(d) )
+'''
